1e66a5af504112c0ad2229ffb790b073b7821432
maritzelmv <marivaquiermmv@gmail.com>
2022-03-31 13:48:52 -0600

    EXAMEN

--- resources/views/vista/index.blade.php
+++ resources/views/vista/index.blade.php
@@ -151,13 +151,13 @@
                 <h2 class="request_text">Lo mas confiable en viajes</h2>
                 <form action="/action_page.php">
                 <div class="form-group">
-                  <input type="text" class="email-bt" placeholder="PICKUP" name="Name">
+                  <input type="text" class="email-bt" placeholder="Lugar a recoger" name="Name">
                 </div>
                 <div class="form-group">
-                  <input type="text" class="email-bt" placeholder="DROP" name="Email">
+                  <input type="text" class="email-bt" placeholder="Lugar de destino" name="Email">
                 </div>
                 <div class="form-group">
-                  <input type="text" class="email-bt" placeholder="WHEN" name="Email">
+                  <input type="text" class="email-bt" placeholder="Cuando" name="Email">
                 </div>
                   </form>
                   </div> 
@@ -190,8 +190,7 @@
               <div class="taxi_main">
                 <div class="round_1">01</div>
                 <h2 class="carol_text">ECATEPEC</h2>
-                <p class="reader_text">act that a reader will be<br> 
-                distracted </p>
+                <p class="reader_text">Llega a cualquier destino de forma segura </p>
                 <div class="images_2"><a href="#"><img src="images/img-2.png"></a></div>
               </div>
             </div>
@@ -199,8 +198,7 @@
               <div class="taxi_main">
                 <div class="round_1">02</div>
                 <h2 class="carol_text">TECAMAC</h2>
-                <p class="reader_text">act that a reader will be<br> 
-                distracted </p>
+                <p class="reader_text">Lllega de la manera mas rapida</p>
                 <div class="images_2"><img src="images/img-2.png"></a></div>
               </div>
             </div>
@@ -208,8 +206,7 @@
               <div class="taxi_main">
                 <div class="round_1">03</div>
                 <h2 class="carol_text">COACALCO</h2>
-                <p class="reader_text">act that a reader will be<br> 
-                distracted </p>
+                <p class="reader_text">Acceso a cualquier plaza </p>
                 <div class="images_2"><img src="images/img-2.png"></a></div>
               </div>
             </div>
@@ -222,7 +219,7 @@
     <div id="booking" class="ride_section layout_padding">
       <div class="container">
         <div class="ride_main">
-          <h1 class="ride_text">Why Ride <span style="color: #f4db31;">With Uloax</span></h1>
+          <h1 class="ride_text">¿Por qué <span style="color: #f4db31;">FASTY?</span></h1>
       </div>
         </div>
     </div>
@@ -233,45 +230,21 @@
             <div class="image_3"><img src="images/img-3.png"></div>
           </div>
           <div class="col-sm-8">
-            <h1 class="cabe_text">Uloax for Every Pocket</h1>
+            <h1 class="cabe_text">Cuidamos de tu dinero</h1>
             <p class="long_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as It is a long established fact that a reader will be distracted by the readable c</p>
-            <div class="book_bt"><a href="#">BOOK NOW</a></div>
+            <div class="book_bt"><a href="#">RESERVA</a></div>
           </div>
         </div>
       </div>
       <div class="container">
         <div class="row">
           <div class="col-sm-8">
-            <h1 class="secure_text">Secure and Safer Rides</h1>
+            <h1 class="secure_text">Nos preocupa de seguridad</h1>
             <p class="long_text_2">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as It is a long established fact that a reader will be distracted by the readable c</p>
-            <div class="book_bt_2"><a href="#">BOOK NOW</a></div>
+            <div class="book_bt_2"><a href="#">RESERVA</a></div>
           </div>
           <div class="col-sm-4">
             <div class="image_3"><img src="images/img-4.png"></div>
-          </div>
-        </div>
-      </div>
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-4">
-            <div class="image_3"><img src="images/img-5.png"></div>
-          </div>
-          <div class="col-sm-8">
-            <h1 class="cabe_text">Uloax for Every Pocket</h1>
-            <p class="long_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as It is a long established fact that a reader will be distracted by the readable c</p>
-            <div class="book_bt"><a href="#">BOOK NOW</a></div>
-          </div>
-        </div>
-      </div>
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-8">
-            <h1 class="secure_text">Secure and Safer Rides</h1>
-            <p class="long_text_2">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as It is a long established fact that a reader will be distracted by the readable c</p>
-            <div class="book_bt_2"><a href="#">BOOK NOW</a></div>
-          </div>
-          <div class="col-sm-4">
-            <div class="image_3"><img src="images/img-5.png"></div>
           </div>
         </div>
       </div>
@@ -284,7 +257,7 @@
           <div class="row">
             <div class="col-sm-4"></div>
             <div class="col-sm-5">
-              <div class="download_text">DOWNLOAD<br><span style="color: #fffcf4;">OUR APP TODAY</span></div>
+              <div class="download_text">DESCARGA<br><span style="color: #fffcf4;">NUESTRA APP</span></div>
             </div>
             <div class="col-sm-3">
               <div class="image_7"><img src="images/img-7.png"></div>
@@ -300,30 +273,24 @@
           <div class="footer_section_2">
             <div class="row">
                 <div class="col-sm-6 col-md-6 col-lg-3">
-                  <h2 class="account_text">Address</h2>
-                  <p class="ipsum_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, </p>
+                  <h2 class="account_text">Ubicación</h2>
+                  <p class="ipsum_text">Estamos ubicados en todos los limites de los municipios en los que contamos con rutas.</p>
                 </div>
                 <div class="col-sm-6 col-md-6 col-lg-3">
                   <h2 class="account_text">Links</h2>
-                  <div class="image-icon"><img src="images/bulit-icon.png"><span class="fb_text"><a href="#">Home</span></a></div>
-                <div class="image-icon"><img src="images/bulit-icon.png"><span class="fb_text"><a href="#">About</span></a></div>
-                <div class="image-icon"><img src="images/bulit-icon.png"><span class="fb_text"><a href="#">Taxi</span></a></div>
-                <div class="image-icon"><img src="images/bulit-icon.png"><span class="fb_text"><a href="#">Booking</span></a></div>
-                <div class="image-icon"><img src="images/bulit-icon.png"><span class="fb_text"><a href="#">Contact Us</span></a></div>
+                  <div class="image-icon"><img src="images/bulit-icon.png"><span class="fb_text"><a href="#">Reserva
+
+                  </span></a></div>
+                <div class="image-icon"><img src="images/bulit-icon.png"><span class="fb_text"><a href="#booking">Acerca</span></a></div>
+                <div class="image-icon"><img src="images/bulit-icon.png"><span class="fb_text"><a href="#taxi">Taxi</span></a></div>
+                <div class="image-icon"><img src="images/bulit-icon.png"><span class="fb_text"><a href="#contact">Contacto</span></a></div>
                 </div>
                 <div class="col-sm-6 col-md-6 col-lg-3">
-                <h2 class="account_text">Follow Us</h2>
-                <div class="image-icon"><img src="images/fb-icon.png"><span class="fb_text"><a href="#">Facebook</a></span></div>
-                <div class="image-icon"><img src="images/twitter-icon.png"><span class="fb_text"><a href="#">Twitter</a></span></div>
-                <div class="image-icon"><img src="images/in-icon.png"><span class="fb_text"><a href="#">Linkedin</a></span></div>
-                <div class="image-icon"><img src="images/youtube-icon.png"><span class="fb_text"><a href="#">Youtube</a></span></div>            
-                <div class="image-icon"><img src="images/instagram-icon.png"><span class="fb_text"><a href="#">Instagram</a></span></div>
+                <h2 class="account_text">Siguenos</h2>
+                <div class="image-icon"><img src="images/fb-icon.png"><span class="fb_text"><a href="#https://www.facebook.com/">Facebook</a></span></div>
+                <div class="image-icon"><img src="images/twitter-icon.png"><span class="fb_text"><a href="#https://twitter.com/?lang=es">Twitter</a></span></div>           
+                <div class="image-icon"><img src="images/instagram-icon.png"><span class="fb_text"><a href="#https://www.instagram.com/">Instagram</a></span></div>
                 </div>
-          <div class="col-sm-6 col-md-6 col-lg-3">
-            <h2 class="adderess_text">Newsletter</h2>
-            <input type="" class="email_text" placeholder="Enter Your Email" name="Enter Your Email">
-            <button class="subscribr_bt">SUBSCRIBE</button>
-          </div>
           </div>
           </div>
           </div>
